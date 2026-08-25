eec77eab148ea7875db2d35387004e822c730987
Olem93 <ole.overdal@gmail.com>
2026-08-25 14:46:13 +0200

    final?
    blah

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let autoChange = 1;
+let timeOut;
+let seconds = 1000;
 
 function changeColor(color1, color2, color3) {
   document.getElementById("redLight").style.background = color1;
@@ -6,24 +8,35 @@ function changeColor(color1, color2, color3) {
   document.getElementById("greenLight").style.background = color3;
 }
 
-function autoChangeSystem(color1, color2, color3) {
+function autoChangeSystem() {
   if (autoChange === 1) {
-    document.getElementById("redLight").style.background = color1;
+    document.getElementById("redLight").style.background = "red";
     document.getElementById("yellowLight").style.background = "#242424";
     document.getElementById("greenLight").style.background = "#242424";
     autoChange = 2;
-    setTimeout(autoChangeSystem, 1000);
+    seconds = 5000;
   } else if (autoChange === 2) {
-    document.getElementById("redLight").style.background = color1;
-    document.getElementById("yellowLight").style.background = color2;
+    document.getElementById("redLight").style.background = "red";
+    document.getElementById("yellowLight").style.background = "yellow";
     document.getElementById("greenLight").style.background = "#242424";
     autoChange = 3;
-    setTimeout(autoChangeSystem, 1000);
-  } else {
+    seconds = 1000;
+  } else if (autoChange === 3) {
     document.getElementById("redLight").style.background = "#242424";
     document.getElementById("yellowLight").style.background = "#242424";
-    document.getElementById("greenLight").style.background = color3;
+    document.getElementById("greenLight").style.background = "green";
+    autoChange = 4;
+    seconds = 5000;
+  } else {
+    document.getElementById("redLight").style.background = "#242424";
+    document.getElementById("yellowLight").style.background = "yellow";
+    document.getElementById("greenLight").style.background = "#242424";
     autoChange = 1;
-    setTimeout(autoChangeSystem, 1000);
+    seconds = 2000;
   }
+  timeOut = setTimeout(autoChangeSystem, seconds);
+}
+
+function stopTimeout() {
+  clearTimeout(timeOut);
 }
